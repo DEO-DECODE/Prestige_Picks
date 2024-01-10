da5f328c5c1d3758f748a95353b75b7792a4a5d7
Deo Raj <d.jrajsingh81@gmail.com>
2024-01-10 19:24:58 +0530

    User Profile Update Done

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -3,18 +3,19 @@ import UserMenu from "../../components/Layout/UserMenu";
 import Layout from "./../../components/Layout/Layout";
 import { useAuth } from "../../context/auth";
 import toast from "react-hot-toast";
-import axios from "axios";
+
 const Profile = () => {
-  //context
+  // context
   const [auth, setAuth] = useAuth();
-  //state
+
+  // state
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phone, setPhone] = useState("");
   const [address, setAddress] = useState("");
 
-  //get user data
+  // get user data
   useEffect(() => {
     const { email, name, phone, address } = auth?.user;
     setName(name);
@@ -26,15 +27,26 @@ const Profile = () => {
   // form function
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     try {
-      const { data } = await axios.put("/api/v1/auth/profile", {
-        name,
-        email,
-        password,
-        phone,
-        address,
+      const response = await fetch("/api/v1/auth/profile", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.token}`,
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+          phone,
+          address,
+        }),
       });
-      if (data?.errro) {
+
+      const data = await response.json();
+
+      if (data?.error) {
         toast.error(data?.error);
       } else {
         setAuth({ ...auth, user: data?.updatedUser });
@@ -49,6 +61,7 @@ const Profile = () => {
       toast.error("Something went wrong");
     }
   };
+
   return (
     <Layout title={"Your Profile"}>
       <div className="container-fluid m-3 p-3 dashboard">
